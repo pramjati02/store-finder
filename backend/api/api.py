efc0abd870fe9604abd9d6e9bb89963ceefdce47
unknown <pramatyajati87@gmail.com>
2025-10-31 09:39:36 +0000

    Added feature to locate nearby stores based on inputted address

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from models import models 
 from schemas import schemas
+import math
+import requests
 
 # db: Session = session that is required to communicate with the database 
 
@@ -14,3 +16,32 @@ def create_store(db: Session, store: schemas.StoreCreate):
 
 def get_stores(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Store).offset(skip).limit(limit).all()
+
+# Convert address to coordinates using OpenStreetMap (Nominatim), retrieving longitude and latitude
+def geocode_address(address: str):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {
+        "q": address,
+        "format": "json",
+        "limit": 1
+    }
+
+    response = requests.get(url, params=params, headers={"User-Agent": "store-finder"})
+    data = response.json()
+    if not data:
+        return None
+    lat = float(data[0]["lat"])
+    lon =  float(data[0]["lon"])
+    return lat, lon 
+
+# calculate distance between two longitude and latitude points (Haversine)
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371 # Earth radius in kilometers 
+    
+    # calculate difference between longitudes and latitudes
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c  # distance in km
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from models import models
 from schemas import schemas
@@ -24,4 +24,37 @@ def read_stores(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @app.post("/stores/", response_model=schemas.StoreRead)
 def create_store(store: schemas.StoreCreate, db: Session = Depends(get_db)):
     return api.create_store(db, store)
+
+@app.get("/stores/nearby", response_model = list[schemas.StoreRead])
+def find_nearby_stores(
+    address: str = Query(..., description = "User inputted address or postcode"),
+    radius_km: float = 2,
+    db: Session = Depends(get_db)
+):
+    # Convert address to coordinates
+    coords = api.geocode_address(address)
+    if not coords:
+        raise HTTPException(status_code=404, detail="Address not found")
+    
+    user_lat, user_lon = coords # user inputted address convert to coordinates
+
+    # Query all stores 
+    stores = api.get_stores(db, skip=0, limit=10000)
+
+    # Compute distance for each store
+    nearby_stores = []
+
+    for s in stores:
+        distance = api.haversine(user_lat, user_lon, s.latitude, s.longitude)
+        if distance <= radius_km: # checking if distance is smaller or equal to the minimum radius set out
+            nearby_stores.append((s, distance))
+
+    # sort by distance
+    nearby_stores.sort(key=lambda x:x[1])
+
+    # Return top 10 closest
+    return [s for s, _ in nearby_stores[:10]]
+
+
+
 
